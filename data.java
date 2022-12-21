e9da33be8c9cf7b327dbe5311b2019d54d03c7cd
savaliyarohit <savaliyarohit333@gmail.com>
2022-12-21 17:50:19 +0000

    datatypes1

---
+++ data.java
@@ -0,0 +1,15 @@
+package datatypes1;
+
+public class data {
+    public static void main(String[] args) {
+        int a=20;
+        int c= 40;
+
+        System.out.println(a+c);//60
+        System.out.println(a-c);//-20
+        System.out.println(a*c);//800
+        System.out.println(a/c);//0
+        System.out.println(a++);//20
+        System.out.println(a--);//21
+    }
+}
